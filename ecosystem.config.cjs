29186a7521108f7be4b74a678a2739683981b2fe
sim0511 <simrandeep2012@gmail.com>
2025-03-15 00:38:30 -0700

    initial commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports ={ 
   apps:[
     {
-      name: 'oauth',
+      name: 'master-cleaning',
       script: 'dist/src/index.js',
       instances: 1,
       autorestart: true, // Enable automatic restart
